8315c688eb3b8c2de0112669f20a3a492c9937bb
adeebkor <mabm4@cam.ac.uk>
2022-11-01 15:03:13 +0000

    Update 3d code and remove old 3d code

--- geo2xdmf_3d.py
+++ geo2xdmf_3d.py
@@ -1,60 +1,31 @@
-import numpy as np
 import gmsh
 
+from dolfinx.io import XDMFFile, gmshio
 from mpi4py import MPI
 
-from dolfinx.graph import create_adjacencylist
-from dolfinx.io import (cell_perm_gmsh, extract_gmsh_geometry,
-                        ufl_mesh_from_gmsh, distribute_entity_data,
-                        extract_gmsh_topology_and_markers, XDMFFile)
-from dolfinx.mesh import CellType, create_mesh, meshtags_from_entities
-
 # Initialization
-source_type = "planar"
-fname = f"{source_type}_3d_0.geo"
+fname = f"planar_3d_0"
 gmsh.initialize()
-gmsh.open(fname)
+gmsh.open(f"{fname}.geo")
 
-if MPI.COMM_WORLD.rank == 0:
+mesh_comm = MPI.COMM_WORLD
+model_rank = 0
+
+if mesh_comm.rank == model_rank:
     gmsh.model.mesh.generate(3)
+    # gmsh.model.mesh.setOrder(1)
+    # gmsh.model.mesh.optimize("HighOrder")
 
-    x = extract_gmsh_geometry(gmsh.model)
-    gmsh_cell_id = MPI.COMM_WORLD.bcast(
-        gmsh.model.mesh.getElementType("Hexahedron", 1), root=0)
-    topologies = extract_gmsh_topology_and_markers(gmsh.model)
-    cells = topologies[gmsh_cell_id]["topology"].astype(np.int64)
-    cell_data = topologies[gmsh_cell_id]["cell_data"].astype(np.int32)
 
-    num_nodes = MPI.COMM_WORLD.bcast(cells.shape[1], root=0)
-    gmsh_facet_id = gmsh.model.mesh.getElementType("Quadrangle", 1)
-    marked_facets = topologies[gmsh_facet_id]["topology"].astype(np.int64)
-    facet_values = topologies[gmsh_facet_id]["cell_data"].astype(np.int32)
+msh, ct, ft = gmshio.model_to_mesh(gmsh.model, mesh_comm, model_rank, gdim=3)
+msh.name = fname
+ct.name = f"{msh.name}_cells"
+ft.name = f"{msh.name}_facets"
 
-else:
-    gmsh_cell_id = MPI.COMM_WORLD.bcast(None, root=0)
-    num_nodes = MPI.COMM_WORLD.bcast(None, root=0)
-    cells, x = np.empty([0, num_nodes]), np.empty([0, 3])
-    marked_facets = np.empty((0, 3), dtype=np.int64)
-    facet_values = np.empty((0, ), dtype=np.int32)
-
-domain = ufl_mesh_from_gmsh(gmsh_cell_id, 3)
-gmsh_hexahedron8 = cell_perm_gmsh(CellType.hexahedron, 8)
-cells = cells[:, gmsh_hexahedron8]
-
-mesh = create_mesh(MPI.COMM_WORLD, cells, x, domain)
-mesh.name = f"{source_type}3d"
-
-gmsh_quadrangle4 = cell_perm_gmsh(CellType.quadrilateral, 4)
-marked_facets = marked_facets[:, gmsh_quadrangle4]
-
-entities, values = distribute_entity_data(mesh, 2, marked_facets, facet_values)
-mesh.topology.create_connectivity(2, 0)
-mt = meshtags_from_entities(
-    mesh, 2, create_adjacencylist(entities), np.int32(values))
-mt.name = f"{source_type}3d_boundaries"
-
-with XDMFFile(MPI.COMM_WORLD, "mesh.xdmf", "w") as file:
-    file.write_mesh(mesh)
-    mesh.topology.create_connectivity(2, 3)
-    file.write_meshtags(
-        mt, geometry_xpath=f"/Xdmf/Domain/Grid[@Name='{source_type}3d']/Geometry")
+with XDMFFile(msh.comm, "mesh.xdmf", "w") as file:
+    file.write_mesh(msh)
+    msh.topology.create_connectivity(2, 3)
+    file.write_meshtags(ct, 
+        geometry_xpath=f"/Xdmf/Domain/Grid[@Name='{msh.name}']/Geometry")
+    file.write_meshtags(ft, 
+        geometry_xpath=f"/Xdmf/Domain/Grid[@Name='{msh.name}']/Geometry")
